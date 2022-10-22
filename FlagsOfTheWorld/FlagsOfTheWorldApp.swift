5515a36b178470d268a5f25f917f7f38bdbf45f8
CodeByDzyne <wdenley@dzynesolutions.com>
2022-10-22 11:33:45 +1100

    Initial Commit

---
+++ FlagsOfTheWorld/FlagsOfTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsOfTheWorldApp.swift
+//  FlagsOfTheWorld
+//
+//  Created by Wealthy Waz on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagsOfTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
